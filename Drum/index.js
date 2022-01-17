b709673f21b5f20c917a38f366d8f16d657f47a4
Vinicius Inomata <89132503+viniginomata@users.noreply.github.com>
2022-01-17 17:11:17 +0900

    beautify code

--- Drum/index.js
+++ Drum/index.js
@@ -1,15 +1,15 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-for(let i = 0 ; i < numberOfDrumButtons; i++)  
+for (let i = 0; i < numberOfDrumButtons; i++)
 
-document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    let buttonInnerHTML = this.innerHTML /* <<< This variable became the text content by using innerHTML. If we write console.log(buttonInnerHTML) the pc give to us only the text*/
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        let buttonInnerHTML = this.innerHTML /* <<< This variable became the text content by using innerHTML. If we write console.log(buttonInnerHTML) the pc give to us only the text*/
 
-    playSound(buttonInnerHTML);
+        playSound(buttonInnerHTML);
 
-    buttonAnimation(buttonInnerHTML)
-     });
-    
-document.addEventListener("keypress", function(event){
+        buttonAnimation(buttonInnerHTML)
+    });
+
+document.addEventListener("keypress", function(event) {
     playSound(event.key);
 
     buttonAnimation(event.key);
@@ -18,50 +18,51 @@ document.addEventListener("keypress", function(event){
 
 // Every button has a event listener. Now I have to identify which key was pressed
 
-    function playSound(key){
+function playSound(key) {
 
-        switch (key){
-            case "w":
-                let audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
+    switch (key) {
+        case "w":
+            let audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
             break;
-            case "a":
-                let audio1 = new Audio("sounds/tom-2.mp3");
-                audio1.play();
+        case "a":
+            let audio1 = new Audio("sounds/tom-2.mp3");
+            audio1.play();
             break;
-            case "s":
-                let audio2 = new Audio("sounds/tom-3.mp3");
-                audio2.play();
+        case "s":
+            let audio2 = new Audio("sounds/tom-3.mp3");
+            audio2.play();
             break;
-            case "d":
-                let audio3 = new Audio("sounds/tom-4.mp3");
-                audio3.play();
+        case "d":
+            let audio3 = new Audio("sounds/tom-4.mp3");
+            audio3.play();
             break;
-            case "j":
-                let audio4 = new Audio("sounds/crash.mp3");
-                audio4.play();
+        case "j":
+            let audio4 = new Audio("sounds/crash.mp3");
+            audio4.play();
             break;
-            case "k":
-                let audio5 = new Audio("sounds/kick-bass.mp3");
-                audio5.play();
+        case "k":
+            let audio5 = new Audio("sounds/kick-bass.mp3");
+            audio5.play();
             break;
-            case "l":
-                let audio6 = new Audio("sounds/snare.mp3");
-                audio6.play();
+        case "l":
+            let audio6 = new Audio("sounds/snare.mp3");
+            audio6.play();
             break;
 
-            default:console.log(buttonInnerHTML);
-        }
+        default:
+            console.log(buttonInnerHTML);
     }
+}
 
 
 
- function buttonAnimation(currentKey) {
-    let activeButton = document.querySelector("."+ currentKey);
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey);
 
     activeButton.classList.add("pressed")
 
-    setTimeout(function(){
+    setTimeout(function() {
         activeButton.classList.remove("pressed");
-    },100);
+    }, 100);
 }
